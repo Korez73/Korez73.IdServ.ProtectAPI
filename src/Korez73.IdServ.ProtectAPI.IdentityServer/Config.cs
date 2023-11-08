79ef272a6f99dd8c552b075b850402187e56f1bb
Jamie Weldon <jamie.weldon@outlook.com>
2023-11-08 08:44:34 -0600

    Add the API project

--- src/Korez73.IdServ.ProtectAPI.IdentityServer/Config.cs
+++ src/Korez73.IdServ.ProtectAPI.IdentityServer/Config.cs
@@ -12,11 +12,25 @@ public static class Config
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new List<ApiScope>
-            { 
-                new ApiScope("Korez73.IdServ.ProtectAPI.API", "Korez73.IdServ.ProtectAPI.API")
-            };
+        { 
+            new ApiScope("Korez73.IdServ.ProtectAPI.API", "Korez73.IdServ.ProtectAPI.API")
+        };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+        new List<Client>
+        { 
+            new Client
+            {
+                ClientId = "Korez73.IdServ.ProtectAPI.Client",
+                //no interactive user, use the clientid/secret for authN
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //secret for authN
+                ClientSecrets = 
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = {"Korez73.IdServ.ProtectAPI.API"}
+            }
+
+        };
 }
