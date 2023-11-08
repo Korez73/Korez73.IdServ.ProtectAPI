45624f0e55cec71445ebc84773d70efe058c67b8
Jamie Weldon <jamie.weldon@outlook.com>
2023-11-08 06:46:22 -0600

    Fix project naming

---
+++ src/Korez73.IdServ.ProtectAPI.IdentityServer/Config.cs
@@ -0,0 +1,22 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Korez73.IdServ.ProtectAPI.IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+            { 
+                new ApiScope("Korez73.IsServ.ProtectAPI.API", "Korez73.IsServ.ProtectAPI.API")
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+            { };
+}
